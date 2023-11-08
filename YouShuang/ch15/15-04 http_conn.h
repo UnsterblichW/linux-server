f26c056bf50a431eb6de17ef08eac4a4fed1f9a9
SHADOW <49482803+UnsterblichW@users.noreply.github.com>
2023-11-08 12:17:21 +0800

    Update 15-04 http_conn.h 加点注释

--- YouShuang/ch15/15-04 http_conn.h
+++ YouShuang/ch15/15-04 http_conn.h
@@ -23,12 +23,19 @@
 class http_conn
 {
 public:
+    /* 文件名的最大长度 */
     static const int FILENAME_LEN = 200;
+    /* 读缓存区的大小 */
     static const int READ_BUFFER_SIZE = 2048;
+    /* 写缓冲区的大小 */
     static const int WRITE_BUFFER_SIZE = 1024;
-    enum METHOD { GET = 0, POST, HEAD, PUT, DELETE, TRACE, OPTIONS, CONNECT, PATCH };
+    /* 暂且只支持了GET */
+    enum METHOD { GET = 0, POST, HEAD, PUT, DELETE, TRACE, OPTIONS, CONNECT, PATCH }; 
+    /* 解析客户请求时，主状态机所处的各种状态 */
     enum CHECK_STATE { CHECK_STATE_REQUESTLINE = 0, CHECK_STATE_HEADER, CHECK_STATE_CONTENT };
+    /* 服务端处理HTTP请求后可能的结果 */
     enum HTTP_CODE { NO_REQUEST, GET_REQUEST, BAD_REQUEST, NO_RESOURCE, FORBIDDEN_REQUEST, FILE_REQUEST, INTERNAL_ERROR, CLOSED_CONNECTION };
+    /* 行的读取状态 */
     enum LINE_STATUS { LINE_OK = 0, LINE_BAD, LINE_OPEN };
 
 public:
@@ -36,17 +43,18 @@ public:
     ~http_conn(){}
 
 public:
-    void init( int sockfd, const sockaddr_in& addr );
+    void init( int sockfd, const sockaddr_in& addr );  /* 初始化新接受的连接 */
     void close_conn( bool real_close = true );
-    void process();
-    bool read();
-    bool write();
+    void process();  /* 处理客户请求 */
+    bool read();     /* 非阻塞读 */
+    bool write();    /* 非阻塞写 */
 
 private:
-    void init();
-    HTTP_CODE process_read();
-    bool process_write( HTTP_CODE ret );
+    void init();    /* 初始化连接 */
+    HTTP_CODE process_read();                /* 解析HTTP请求 */
+    bool process_write( HTTP_CODE ret );     /* 填充HTTP应答 */
 
+    /* 下面这一组函数会被 process_read 调用以分析HTTP请求 */
     HTTP_CODE parse_request_line( char* text );
     HTTP_CODE parse_headers( char* text );
     HTTP_CODE parse_content( char* text );
@@ -54,6 +62,7 @@ private:
     char* get_line() { return m_read_buf + m_start_line; }
     LINE_STATUS parse_line();
 
+    /* 下面这一组函数会被 process_write 调用以填充HTTP应答 */
     void unmap();
     bool add_response( const char* format, ... );
     bool add_content( const char* content );
@@ -64,32 +73,52 @@ private:
     bool add_blank_line();
 
 public:
+    /* 所有socket上的事件都被注册到同一个epoll内核事件表中，所以将epoll文件描述符设置为静态的 */
     static int m_epollfd;
+    /* 统计客户数量 */
     static int m_user_count;
 
 private:
+    /* 该HTTP连接的socket和对方的socket地址 */
     int m_sockfd;
     sockaddr_in m_address;
 
+    /* 读缓冲区 */
     char m_read_buf[ READ_BUFFER_SIZE ];
+    /* 标识读缓冲中已经读入的客户数据的最后一个字节的下一个位置 */
     int m_read_idx;
+    /* 当前正在分析的字符在读缓存中的位置 */
     int m_checked_idx;
+    /* 当前正在解析的行的起始位置 */
     int m_start_line;
+    /* 写缓冲区 */
     char m_write_buf[ WRITE_BUFFER_SIZE ];
+    /* 写缓存区中待发送的字节数 */
     int m_write_idx;
 
+    /* 主状态机当前所处的状态 */
     CHECK_STATE m_check_state;
+    /* 请求方法 */
     METHOD m_method;
 
+    /* 客户请求的目标文件的完整路径，其内容等于 doc_root + m_url, doc_root 时网站根目录 */
     char m_real_file[ FILENAME_LEN ];
+    /* 客户请求的目标文件的文件名 */
     char* m_url;
+    /* HTTP的协议版本号，我们仅支持HTTP1.1 */
     char* m_version;
+    /* 主机名 */
     char* m_host;
+    /* HTTP请求的消息体的长度 */
     int m_content_length;
+    /* HTTP请求是否要保持连接 */
     bool m_linger;
 
+    /* 客户请求的目标文件被mmap到内存中的起始位置 */
     char* m_file_address;
+    /* 目标文件的状态，通过它我们可以判断文件是否存在、是否为目录、是否可读，并获取文件大小等信息 */
     struct stat m_file_stat;
+    /* 我们将采用 writev 来执行写操作，所以定义下面两个成员，其中 m_iv_count 表示被写内存块的数量 */
     struct iovec m_iv[2];
     int m_iv_count;
 };
